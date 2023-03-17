3f0a849dc93ee19988d1965d42652e3cd7942e44
frncscsrcc <frncscsrcc@gmail.com>
2023-03-17 19:04:18 +0100

    Echo server for E2E localh test

---
+++ cmd/echo.go
@@ -0,0 +1,68 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var echoHost, echoPort string
+var delay int
+
+// echoCmd represents the echo command
+var echoCmd = &cobra.Command{
+	Use:   "echo",
+	Short: "Simple echo server to test the agents locally",
+	Run: func(cmd *cobra.Command, args []string) {
+		handleRequest()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(echoCmd)
+	echoCmd.Flags().StringVar(&echoHost, "host", "0.0.0.0", "listening host")
+	echoCmd.Flags().StringVar(&echoPort, "port", "5000", "listening port")
+	echoCmd.Flags().IntVar(&delay, "delay", 1, "delay in sec")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", ServeHTTP)
+	hostAndPort := echoHost + ":" + echoPort
+	log.Printf("Listening on %s (response delay set to %d sec)", hostAndPort, delay)
+	if err := http.ListenAndServe(hostAndPort, nil); err != nil {
+		panic(err)
+	}
+}
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if delay > 0 {
+		log.Printf("Received request, but waiting %d sec(s).\n", delay)
+		time.Sleep(time.Second * time.Duration(delay))
+	}
+
+	log.Print("New Request")
+	log.Print("Method: " + r.Method)
+	log.Print("Path: " + r.URL.Path)
+	log.Print("Headers:")
+	for name, values := range r.Header {
+		for _, value := range values {
+			log.Printf("\t%s: %s\n", name, value)
+			w.Header().Add(name, value)
+		}
+	}
+	buf := new(strings.Builder)
+	io.Copy(buf, r.Body)
+	log.Print("Body:")
+	log.Print("\t", buf.String())
+	log.Print("-------------")
+	fmt.Fprint(w, buf)
+	r.Body.Close()
+}
